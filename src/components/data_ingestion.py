5981a7f2f5268780203c530c2dcb4501d74fcde4
Hari Prasath AS <hpofficial420@gmail.com>
2024-08-08 14:47:36 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,9 +9,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import train
 from src.components import data_transformation
+from src.components import model_trainer
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 from src.exception import CustomException
 from src.logger import logging
+
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 
 @dataclass
@@ -61,4 +64,8 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initite_model_trainer(train_arr,test_arr))
+                                                                        
+                                                                        
