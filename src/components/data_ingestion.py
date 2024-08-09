43e2c3f98250e45fa7b3d162b80334f00266ffb6
Hari Prasath AS <hpofficial420@gmail.com>
2024-08-09 10:34:15 +0530

    UI and Predction has been implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,71 +1,58 @@
-from email import header
-from genericpath import exists
-from operator import index
 import os
-from re import S
 import sys
-from dataclasses import dataclass
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from xgboost import train
-from src.components import data_transformation
-from src.components import model_trainer
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
 from src.exception import CustomException
 from src.logger import logging
+import pandas as pd
 
-from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','data.csv')
-
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
-    
+
     def initiate_data_ingestion(self):
-        '''
-        This will read from the data base
-        '''
-        logging.info("Entered The Data Ingestion method")
+        logging.info("Entered the data ingestion method or component")
         try:
-            '''
-            Here we can call from mongoDB or any cloud DataBases
-            '''
-            df=pd.read_csv('/home/hp/Documents/Projects/ml-project/src/notebook/data/stud.csv')
-            logging.info("Exported the dataset as Dataframe")
-            
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #If folder is already there then exist_ok will keep the same folder instead of creating
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            logging.info("Train Test split Initiated")
-            train_set,test_set = train_test_split(df,test_size=0.1,random_state=42)
-            
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)      
-            
-            logging.info("Ingestion of Data is completed")
-            
-            return (
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Inmgestion of the data iss completed")
+
+            return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
-            )
-        
+                self.ingestion_config.test_data_path)
+            
         except Exception as e:
             raise CustomException(e,sys)
-
-if __name__ == "__main__":
+        
+if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-    
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
     modeltrainer=ModelTrainer()
-    print(modeltrainer.initite_model_trainer(train_arr,test_arr))
-                                                                        
-                                                                        
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,45 +1,42 @@
-from cgi import test
-import os 
-from pyexpat import model
-from sklearn.linear_model import LinearRegression
+import os
 import sys
 from dataclasses import dataclass
-from turtle import mode
 
 from catboost import CatBoostRegressor
-from matplotlib.mathtext import RasterParse
-from numpy import sort
-from pandas import Categorical
-from sklearn import base, preprocessing
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
-from sklearn.model_selection import ParameterSampler
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trainer_model_file_path = os.path.join("artifacts","model.pkl")
-    
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-    
-    def initite_model_trainer(self,train_array,test_array):
-        """
-        This function is responsible for initiating Model Training.
-        """
+
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Split training and testing input data.")
-            X_train,y_train,X_test,y_test= train_array[:,:-1],train_array[:,-1],test_array[:,:-1],test_array[:,-1]
-            
-            
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -49,7 +46,6 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -87,29 +83,37 @@ class ModelTrainer:
                 }
                 
             }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
             
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
-            
-            # To get best model score from dict
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
-            
-            #to get best model name from dict
+
+            ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            
             best_model = models[best_model_name]
-            
-            if best_model_score  < 0.6:
-                raise CustomException("No best model Found.")
-            logging.info("Best found model on both training and testing dataset.")
-            
-            save_object(file_path=self.model_trainer_config.trainer_model_file_path, obj=best_model)
-            
-            predicted =  best_model.predict(X_test)
-            r2_square = r2_score(y_test,predicted)
-            
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
             return r2_square
-        
+            
+
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
